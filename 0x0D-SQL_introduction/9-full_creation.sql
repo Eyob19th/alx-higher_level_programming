7163ce23c8caddee33f7e2e99db1615f5b2a496b
Eyob19th <getacheweyob732@gmail.com>
2022-08-09 13:34:46 -0700

    commiting

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
-tes a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
+-- creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows
 -- creates a new table
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 -- adds new rows to the table
