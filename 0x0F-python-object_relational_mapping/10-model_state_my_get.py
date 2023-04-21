6559c0bda1cc2d1c4aa64c8cd72b2fcd0846522c
root <getacheweyob732@gmail.com>
2023-04-21 22:26:30 +0000

    commited

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,6 @@
-sts the State object with the name passed as argument
+#!/usr/bin/python3
+"""
+Lists the State object with the name passed as argument
 from the database hbtn_0e_6_usa.
 Usage: ./10-model_state_my_get.py <mysql username> /
                                   <mysql password> /
@@ -17,11 +19,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-    if found is False:
-        print("Not found")
+
+
