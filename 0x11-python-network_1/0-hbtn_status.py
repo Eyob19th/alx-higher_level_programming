98743c62f93a0296f72b132aee145f018504e189
root <getacheweyob732@gmail.com>
2023-04-28 16:09:52 +0000

    commited

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,9 @@
+mport urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    body = response.read()
+    print('- type: {}'.format(type(body)))
+    print('- content: {}'.format(body))
+    print('- utf8 content: {}'.format(body.decode('utf-8')))
