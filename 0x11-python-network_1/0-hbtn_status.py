9a5d94b00dadd425a9c9768560c8d236383a769a
root <getacheweyob732@gmail.com>
2023-04-28 16:22:51 +0000

    commited

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,10 +1,29 @@
 #!/usr/bin/python3
-mport urllib.request
+"""
+Fetches https://alx-intranet.hbtn.io/status and displays the body of the response.
+"""
 
-url = 'https://alx-intranet.hbtn.io/status'
+import urllib.request
 
-with urllib.request.urlopen(url) as response:
-    body = response.read()
+
+def fetch_status(url: str) -> str:
+    """
+    Fetches the status of a given URL.
+
+    Args:
+        url (str): The URL to fetch.
+
+    Returns:
+        The response body as a string.
+    """
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        return body.strip()
+
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+
+    body = fetch_status(url)
     print('- type: {}'.format(type(body)))
     print('- content: {}'.format(body))
-    print('- utf8 content: {}'.format(body.decode('utf-8')))
