2a61d7f4be760b8672bcb941c8f02b8796f0ced5
root <getacheweyob732@gmail.com>
2023-04-29 09:14:24 +0000

    task added

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""A script that
+'''
+A script that
 - fetches https://intranet.hbtn.io/status.
 - uses urlib package
-"""
+'''
 
 
 if __name__ == '__main__':
-    import urllib.request
+    from urllib import request
 
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
         content = res.read()
