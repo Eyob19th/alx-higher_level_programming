47efcc11f98f7e75836c9f55bca6f1a5b1421ca1
root <getacheweyob732@gmail.com>
2023-04-29 09:00:36 +0000

    commited

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""This script fetches https://alx-intranet.hbtn.io/status and displays its body"""
+"""A script that
+- fetches https://intranet.hbtn.io/status.
+- uses urlib package
+"""
 
-import urllib.request
 
-if name == "main":
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-body = response.read()
-print("Body response:")
-print("\t- type: {}".format(type(body)))
-print("\t- content: {}".format(body))
-print("\t- utf8 content: {}".format(body.decode('utf-8')))
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+
